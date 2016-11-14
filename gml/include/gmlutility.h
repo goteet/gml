1d1be4422ff332d781d509b55c80088448a96f49
goteet <goteet@126.com>
2016-11-14 10:44:35 +0800

    util constexpr

--- gml/include/gmlutility.h
+++ gml/include/gmlutility.h
@@ -9,7 +9,7 @@ namespace gml
 	bool fequal(float a, float b);
 
 	template<typename T>
-	inline T lerp(T left, T right, float fac)
+	constexpr inline T lerp(T left, T right, float fac)
 	{
 		return left * (1.0f - fac) + right * fac;
 	}
