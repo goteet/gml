28581bc4b801fd470c2069b62d3492f49456128f
goteet <goteet@126.com>
2016-11-14 17:08:43 +0800

    rect in single file.

--- gml/include/gmlrect.h
+++ gml/include/gmlrect.h
@@ -26,57 +26,99 @@ namespace gml
 	class rect
 	{
 	public:
-		bool operator==(const rect& other) const;
+		friend constexpr bool operator==(const rect& lhs, const rect& rhs) { return (&lhs == &rhs) || ((lhs.m_pos == rhs.m_pos) && (lhs.m_size == rhs.m_size)); }
 
-		int left() const;
+		constexpr int left() const { return m_pos.x; }
 
-		int right() const;
+		constexpr int right() const { return left() + width(); }
 
-		int top() const;
+		constexpr int top() const { return m_pos.y; }
 
-		int bottom() const;
+		constexpr int bottom() const { return  top() + height(); }
 
-		int width() const;
+		constexpr int width() const { return m_size.x; }
 
-		int height() const;
+		constexpr int height() const { return m_size.y; }
 
-		coord center() const;
+		constexpr coord center() const { return coord((left() + right()) / 2, (top() + bottom()) / 2); }
 
-		coord position() const;
+		constexpr coord position() const { return m_pos; }
 
-		coord size() const;
+		constexpr coord size() const { return m_size; }
 
-		void set_width(int w);
+		inline void set_width(int w) { m_size.x = w > 0 ? w : 0; }
 
-		void set_height(int h);
+		inline void set_height(int h) { m_size.y = h > 0 ? h : 0; }
 
-		void set_left(int x);
+		inline void set_pos(int x, int y) { m_pos.set(x, y); }
 
-		void set_right(int x);
+		inline void set_pos(const coord& pos) { m_pos = pos; }
 
-		void set_top(int y);
+		inline void set_size(int w, int h) { m_size.set(w, h); }
 
-		void set_bottom(int y);
+		inline void set_size(const coord& size) { m_size = size; }
 
-		void set_pos(int x, int y);
+		constexpr bool contains(int x, int y) const { return !(x < left() || x > right() || y < top() || y > bottom()); }
 
-		void set_pos(const coord& pos);
+		constexpr bool contains(const coord& point) const { return contains(point.x, point.y); }
 
-		void set_size(int w, int h);
+		inline it_mode hit_test(const rect& other) const
+		{
+			if (this == &other)
+			{
+				return it_same;
+			}
 
-		void set_size(const coord& size);
+			int intersection_count = get_intersection_count(other, *this);
+			if (intersection_count == 4)
+			{
+				return it_inside;
+			}
+			// Impossible 3.
+			else if (intersection_count == 2 || intersection_count == 1)
+			{
+				return it_hit;
+			}
+			else
+			{
+				intersection_count = get_intersection_count(*this, other);
+				if (intersection_count == 4)
+				{
+					return it_contain;
+				}
+				// Impossible 3.
+				else if (intersection_count == 2) // Impossible 1
+				{
+					return it_hit;
+				}
+				else
+				{
+					return it_none;
+				}
+			}
+		}
 
-		bool contains(int x, int y) const;
+		inline void move(int offsetx, int offsety) { m_pos.move(offsetx, offsety); }
 
-		bool contains(const coord& point) const;
-
-		it_mode hit_test(const rect& other) const;
-
-		void move(int offsetx, int offsety);
-
-		void enlarge(int offsetw, int offseth);
+		inline void enlarge(int offsetw, int offseth)
+		{
+			m_size.move(offsetw, offseth);
+			if (m_size.x < 0) m_size.x = 0;
+			if (m_size.y < 0) m_size.y = 0;
+		}
 
 	private:
+		inline int get_intersection_count(const rect& container, const rect& to_check) const
+		{
+			int intersect_other = 0;
+
+			if (container.contains(to_check.left(), to_check.top()))		intersect_other++;
+			if (container.contains(to_check.left(), to_check.bottom()))		intersect_other++;
+			if (container.contains(to_check.right(), to_check.top()))		intersect_other++;
+			if (container.contains(to_check.right(), to_check.bottom()))	intersect_other++;
+			return intersect_other;
+		}
+
 		coord m_pos;
 		coord m_size;
 	};
