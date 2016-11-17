a75fbf1bde7649d76c124ffbeba9aaaa3edbca3d
goteet <goteet@126.com>
2016-11-17 10:02:09 +0800

    remove unnecessary file.

--- gml_test/stdafx.h
+++ gml_test/stdafx.h
@@ -2,10 +2,13 @@
 // 或是经常使用但不常更改的
 // 特定于项目的包含文件
 //
-
 #pragma once
 
-#include "targetver.h"
+// 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
+
+// 如果要为以前的 Windows 平台生成应用程序，请包括 WinSDKVer.h，并将
+// 将 _WIN32_WINNT 宏设置为要支持的平台，然后再包括 SDKDDKVer.h。
+#include <SDKDDKVer.h>
 
 // 用于 CppUnitTest 的头文件
 #include <CppUnitTest.h>
