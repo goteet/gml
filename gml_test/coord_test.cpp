6fe003b52c2cdd2d2d2f0d48517c051fddcedba6
goteet <goteet@126.com>
2016-11-18 14:07:54 +0800

    coord testcases

--- gml_test/coord_test.cpp
+++ gml_test/coord_test.cpp
@@ -3,8 +3,35 @@
 TEST_CLASS(CoordinateTest)
 {
 public:
-	TEST_METHOD(Coordinate)
+	TEST_METHOD(CoordinateComparisionTest)
 	{
+		coord a;
+		coord b;
+		GML_IS_TRUE(a == b);
+		GML_IS_FALSE(a != b);
 
+		a.set(3, 2);
+		GML_IS_TRUE(a != b);
+		GML_IS_FALSE(a == b);
+
+		b.move(3, 2);
+		GML_IS_TRUE(a == b);
+		GML_IS_FALSE(a != b);
+	}
+
+
+	TEST_METHOD(CoordinateBehaviorTest)
+	{
+		coord a;
+		GML_FEQUAL(0, a.x);
+		GML_FEQUAL(0, a.y);
+
+		a.set(1, 2);
+		GML_FEQUAL(1, a.x);
+		GML_FEQUAL(2, a.y);
+
+		a.move(-1, -2);
+		GML_FEQUAL(0, a.x);
+		GML_FEQUAL(0, a.y);
 	}
 };
